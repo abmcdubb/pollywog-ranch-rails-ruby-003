96a7efb361c56782e4984d30c9a470b4a49cb1aa
abmcdubb <abmcwilliams@gmail.com>
2014-03-12 09:56:50 -0400

    initial file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollywogRanchRailsRuby003::Application.config.secret_key_base = '7f52470da9b5a2e7989d8b517c9070ce9206bfc7f32f1a34bcdd6b16f56a0efc829f0ffa5759e3e9adaf16c20c34173a7c1a6690d7461ac0346f8442ccaf3b34'
